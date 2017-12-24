dc6805bf88af8eab1110aa3642f1d17d2d509992
Ruslan J <leonemsolis@gmail.com>
2017-12-24 19:16:00 +0900

    Ready for deploying to Heroky

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const hbs = require("hbs");
 const fs = require("fs");
 
 const app = express();
+const port = process.env.PORT || 3000;
 
 hbs.registerPartials(__dirname + "/views/partials");
 
@@ -51,6 +52,6 @@ app.get("/bad", (req, res) => {
     });
 });
 
-app.listen(process.env.PORT || 3000, () => {
-    console.log("Server is up")
+app.listen(port, () => {
+    console.log("Server is up on port: ", port);
 });
